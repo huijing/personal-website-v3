57fa3b039f78fca55124338806da99a3767a295c
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2025-10-06 22:24:13 +0800

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   site: "https://chenhuijing.com",
   integrations: [mdx(), sitemap()],
+  adapter: netlify(),
 });
